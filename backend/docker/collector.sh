fc59dcc23297c87fb62097eb8e6c39c9c032a507
thijs <thijs@getobjectiv.com>
2021-04-30 15:58:39 +0200

    disable python buffering

--- backend/docker/collector.sh
+++ backend/docker/collector.sh
@@ -2,6 +2,9 @@
 
 . venv/bin/activate
 export PYTHONPATH=.:$PYTHONPATH
+# Tell python not to buffer any output to stdout and stderr. Not setting this makes any debugging almost
+# impossible
+export PYTHONUNBUFFERED=1
 
 # Run gunicorn. $USER and $PORT are set in the Dockerfile
 exec gunicorn --config /etc/gunicorn.conf.py \
