f1a82bbd7536e566acc068b4b145936c497c18c8
sdirosa <surai@objectiv.ai>
2021-05-25 13:21:21 +0200

    WebTracker basic README

--- tracker/trackers/web/README.md
+++ tracker/trackers/web/README.md
@@ -1,3 +1,48 @@
-# objectiv-tracker
+# Objectiv WebTracker 
+[PLACEHOLDER: Objectiv WebTracker introductory text]
 
-Tracker monorepo architecture
+---
+# Installing
+To install the most recent stable version:
+
+### yarn
+```sh
+yarn add @objectiv/tracker-web
+```
+
+### npm
+```sh
+npm install @objectiv/tracker-web
+```
+
+# Usage
+
+### ESModule
+```javascript
+import { WebTracker } from '@objectiv/tracker-web';
+const webTracker = new WebTracker({ endpoint: '/collector' });
+```
+
+### CommonJS
+```javascript
+const objectivCore = require('@objectiv/tracker-web');
+const webTracker = new objectivCore.Tracker({ endpoint: '/collector' });
+```
+
+### IIFE
+```html
+<script src="dist/index.iife.js"></script>
+<script>
+  const webTracker = new objectivCore.Tracker({ endpoint: '/collector' });
+</script>
+```
+
+### AMD
+```html
+<script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js"></script>
+<script>
+  window.requirejs(['dist/index'], function(objectivCore) {
+    const webTracker = new objectivCore.Tracker({ endpoint: '/collector' });
+  });
+</script>
+```
--- tracker/trackers/web/rollup.config.js
+++ tracker/trackers/web/rollup.config.js
@@ -6,14 +6,29 @@ import filesize from 'rollup-plugin-filesize';
 import sizes from 'rollup-plugin-sizes';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      dir: 'dist',
-      format: 'umd',
-      name: 'objectivTrackerWeb',
-    },
-  ],
-  plugins: [nodeResolve(), commonjs(), ts(), cleanup(), terser(), sizes(), filesize()],
-};
+const commonPlugins = [nodeResolve(), commonjs(), ts()];
+const minificationPlugins = [cleanup(), terser()];
+const statsPlugins = [sizes(), filesize()];
+
+const makeOutput = (format, isMinified, isDefault) => ({
+  file: `dist/index${!isDefault ? `.${format}` : ''}${isMinified ? '.min' : ''}.js`,
+  format: format,
+  name: 'objectivCore',
+  sourcemap: true,
+});
+
+export default [
+  // UMD, CJS, ES, IIFE
+  {
+    input: './src/index.ts',
+    output: [makeOutput('umd', false, true), makeOutput('cjs'), makeOutput('es'), makeOutput('iife')],
+    plugins: [...commonPlugins, ...statsPlugins],
+  },
+
+  // UMD, CJS, ES, IIFE - minified
+  {
+    input: './src/index.ts',
+    output: [makeOutput('umd', true, true), makeOutput('cjs', true), makeOutput('es', true), makeOutput('iife', true)],
+    plugins: [...commonPlugins, ...minificationPlugins, ...statsPlugins],
+  },
+];
