49c92f541afba342ed6b3019178a0c7113427ec8
sdirosa <surai@objectiv.ai>
2021-05-31 09:36:02 +0200

    Fix unit test, list to usableTransports TransportGroup prop rename

--- tracker/trackers/web/tests/WebTracker.test.ts
+++ tracker/trackers/web/tests/WebTracker.test.ts
@@ -34,7 +34,7 @@ describe('WebTracker', () => {
       },
       transport: {
         transportName: 'TransportGroup',
-        list: [
+        usableTransports: [
           {
             transportName: 'TransportSwitch',
             firstUsableTransport: {
