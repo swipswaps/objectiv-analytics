4f5e198b913c5044d6dc4c87baeba40c412f76d4
thijs <thijs@getobjectiv.com>
2021-06-03 13:25:48 +0200

    remove explicit 'public' schema from sql create statements

--- backend/objectiv_backend/create_tables.sql
+++ backend/objectiv_backend/create_tables.sql
@@ -1,20 +1,20 @@
 begin;
 
-create table public.queue_entry (
+create table queue_entry (
     event_id uuid not null,
     insert_order bigserial,
     value json not null,
     primary key(event_id)
 );
 
-create table public.queue_finalize (
+create table queue_finalize (
     event_id uuid not null,
     insert_order bigserial,
     value json not null,
     primary key(event_id)
 );
 
-create table public.data (
+create table data (
     event_id uuid not null,
     day date not null, -- This is for query convenience; a possible sharding key? We might well put an index on this badboy
     moment timestamp not null,
@@ -25,7 +25,7 @@ create table public.data (
 
 create index on data(day);
 
-create table public.nok_data (
+create table nok_data (
     -- perhaps we want to add a field here that states the reason why the data is not ok?
     event_id uuid not null,
     day date not null, -- This is for query convenience; a possible sharding key? We might well put an index on this badboy
@@ -83,21 +83,21 @@ order by session_id, moment
 
 -- used by collector to write incoming events
 create role obj_collector_role noinherit;
-grant select,update,insert on public.queue_entry to obj_collector_role;
+grant select,update,insert on queue_entry to obj_collector_role;
 -- we also add the "worker" permissions here, to make sure
 -- the synchronous mode properly works
-grant insert on public.data, public.nok_data to obj_collector_role;
+grant insert on data, nok_data to obj_collector_role;
 
 -- used by worker to read/write queues
 -- update priv is needed because of the `select for update` queries
 create role obj_worker_role noinherit;
-grant select,update,delete on public.queue_entry to obj_worker_role;
-grant select,update,insert,delete on public.queue_finalize to obj_worker_role;
-grant insert on public.data, public.nok_data to obj_worker_role;
+grant select,update,delete on queue_entry to obj_worker_role;
+grant select,update,insert,delete on queue_finalize to obj_worker_role;
+grant insert on data, nok_data to obj_worker_role;
 
 -- used by for example notebook to query session data
 create role obj_reader_role noinherit;
-grant select on public.data,public.data_with_sessions to obj_reader_role;
+grant select on data,data_with_sessions to obj_reader_role;
 
 
 commit;
