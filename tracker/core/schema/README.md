f58145a8563e6ff51f13506946d73f9e52b164a8
sdirosa <surai@objectiv.ai>
2021-06-04 15:43:41 +0200

    Added some basic documentation about abstracts and how discrimination works

---
+++ tracker/core/schema/README.md
@@ -0,0 +1,16 @@
+# Objectiv Schema TypeScript
+[PLACEHOLDER: Objectiv Schema TypeScript introductory text]
+
+---
+# Installing
+To install the most recent version:
+
+### yarn
+```sh
+yarn add @objectiv/schema --dev
+```
+
+### npm
+```sh
+npm install @objectiv/schema --dev
+```
--- tracker/core/schema/src/abstracts.d.ts
+++ tracker/core/schema/src/abstracts.d.ts
@@ -1,56 +1,146 @@
 import { MediaPlayerContext } from './location_contexts';
 
 /**
- * Abstract Contexts
+ * Abstract Contexts define either properties required by Collectors or internal ones for hierarchical discrimination
+ * purposes.
+ *
+ * For example we never want to mix Location Contexts with Global Contexts and Events may requires specific Contexts
+ * to be present in their Location Stack. Eg. a NavigationContext instead of a generic SectionContext.
+ *
+ * This ensures that Events are carrying the Contexts they require to be meaningful and identifiable.
+ *
+ * TypeScript does not support multiple inheritance, thus we use property based discrimination instead.
+ * For more info: https://www.typescriptlang.org/docs/handbook/2/narrowing.html#discriminated-unions
+ */
+
+/**
+ * All Contexts inherit from AbstractContext. It defines the bare minimum properties every Context must implement.
+ *
+ * Extending interfaces should narrow `_context_type` type from string to a string literal matching the interface name.
+ *
+ * Examples:
+ *   readonly _context_type: 'SectionContext';
+ *   readonly _context_type: 'DeviceContext';
  */
 export abstract class AbstractContext {
+  /**
+   * A string literal used during serialization. Should always match the Context interface name.
+   */
   readonly _context_type: string;
+
+  /**
+   * A unique string identifier to be combined with `_context_type` for Context instance uniqueness.
+   */
   readonly id: string;
 }
 
+/**
+ * GlobalContexts are used to populate Trackers or Events `globalContexts` properties.
+ * They carry information that is not related to the UI, such as device, platform or business data.
+ */
 export abstract class AbstractGlobalContext extends AbstractContext {
+  /**
+   * Discrimination property
+   */
   readonly _global = true;
 }
 
+/**
+ * LocationContexts are used to populate Trackers or Events `locationStack` properties.
+ * A Location Stack is meant to describe accurately where an Event originated.
+ */
 export abstract class AbstractLocationContext extends AbstractContext {
+  /**
+   * Discrimination property
+   */
   readonly _location = true;
 }
 
+/**
+ * SectionContexts are special LocationContexts representing a logical area of the UI or the system.
+ * They can be often reasoned bout as being containers of other LocationContexts but not the direct targets of Events.
+ */
 export abstract class AbstractSectionContext extends AbstractLocationContext {
+  /**
+   * Discrimination property
+   */
   readonly _section = true;
 }
 
+/**
+ * ItemContexts are special LocationContexts representing interactive elements of the UI or targets in a system.
+ * These elements may trigger both Interactive and Non-Interactive Events. Eg. an Input field or a Button.
+ */
 export abstract class AbstractItemContext extends AbstractLocationContext {
+  /**
+   * Discrimination property
+   */
   readonly _item = true;
 }
 
+/**
+ * ActionContexts are a more specific version of ItemContext specifically meant to describe actionable Items.
+ * These represent interactive elements that will trigger an Interactive Event. Eg. A Button or Link.
+ */
 export abstract class AbstractActionContext extends AbstractItemContext {
+  /**
+   * Discrimination property
+   */
   readonly _action = true;
 }
 
 /**
- * Abstract Events
+ * An interface coupling Location Stack and Global Contexts. Used by Tracker and Events.
  */
 export interface Contexts {
+  /**
+   * A list of Location Contexts. Order matters as they must reconstruct a logical location in the UI or system.
+   */
   readonly locationStack: AbstractLocationContext[];
+
+  /**
+   * A list of Global Contexts. In any order.
+   */
   readonly globalContexts: AbstractGlobalContext[];
 }
 
+/**
+ * Events must provide a `name` and optionally can, but most likely will, carry a list of Location and Global Contexts
+ */
 export abstract class AbstractEvent implements Contexts {
+  /**
+   * A string literal used during serialization. Should always match the Event interface name.
+   */
   readonly event: string;
+
+  /**
+   * The Contexts interface implementation
+   */
   readonly locationStack: AbstractLocationContext[];
   readonly globalContexts: AbstractGlobalContext[];
 }
 
+/**
+ * Events that are not triggered directly by the user. Eg. asynchronous or automatic ones.
+ */
 export abstract class AbstractNonInteractiveEvent extends AbstractEvent {
+  /**
+   * Discrimination property
+   */
   readonly _interactive = false;
 }
 
+/**
+ * Events that are the direct result of a user interaction. Eg. a Button Click
+ */
 export abstract class AbstractInteractiveEvent extends AbstractEvent {
   readonly _interactive = true;
   readonly locationStack: [AbstractSectionContext, ...AbstractLocationContext[]];
 }
 
+/**
+ * A category of Non Interactive Events specifically meant to describe Media Player interactions.
+ */
 export interface AbstractVideoEvent extends AbstractNonInteractiveEvent {
   readonly _video: true;
   readonly locationStack: [MediaPlayerContext, ...AbstractLocationContext[]];
