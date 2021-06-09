6e0c9385c20fe32191c91acb6f1e08a23dd25741
sdirosa <surai@objectiv.ai>
2021-06-09 22:28:53 +0200

    Last two events ready to be tested

--- tracker/trackers/react/src/useTrackApplicationLoaded.ts
+++ tracker/trackers/react/src/useTrackApplicationLoaded.ts
@@ -1,14 +1,12 @@
 import { useTracker } from './ObjectivProvider';
 import { ReactTracker } from './ReactTracker';
-import { useOnMount } from "./helperHooks";
 
 /**
  * Triggers an ApplicationLoadedEvent when the using component mounts for the first time.
  * This hook is meant to be used high up in the Application. Eg: right after initialization and before routing.
  */
-export const useTrackApplicationLoaded = (tracker: ReactTracker = useTracker()) =>
-  useOnMount(() => {
-    console.log(tracker);
-    // TODO need schema
-    //tracker.trackEvent(makeApplicationLoadedEvent());
-  });
+export const useTrackApplicationLoaded = (tracker: ReactTracker = useTracker()) => {
+  console.log(tracker);
+  // TODO need schema
+  //useTrackOnMount(makeApplicationLoadedEvent(), tracker)
+};
--- tracker/trackers/react/src/useTrackVisibility.ts
+++ tracker/trackers/react/src/useTrackVisibility.ts
@@ -1,20 +1,15 @@
 import { useTracker } from './ObjectivProvider';
 import { ReactTracker } from "./ReactTracker";
-import { useOnMount, useOnUnmount } from "./helperHooks";
 
 /**
  * A side effect that triggers SectionVisibleEvent and SectionHiddenEvent as the component mounts and unmounts.
  */
 export const useTrackVisibility = (tracker: ReactTracker = useTracker()) => {
-  useOnMount(() => {
-    // TODO need schema
-    //tracker.trackEvent(makeSectionVisibleEvent());
-    console.log(tracker)
-  });
+  console.log(tracker)
 
-  useOnUnmount(() => {
-    // TODO need schema
-    //tracker.trackEvent(makeSectionHiddenEvent());
-    console.log(tracker)
-  });
+  // TODO need schema
+  //useTrackOnMount(makeSectionVisibleEvent(), tracker);
+
+  // TODO need schema
+  //useTrackOnUnmount(makeSectionHiddenEvent(), tracker);
 }
