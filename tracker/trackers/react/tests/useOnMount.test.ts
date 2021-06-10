f73087031c5e1b84ccc16dc2213f01d82f261376
sdirosa <surai@objectiv.ai>
2021-06-10 08:48:35 +0200

    React testing tools and useOnMount tests

---
+++ tracker/trackers/react/tests/useOnMount.test.ts
@@ -0,0 +1,34 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useOnMount } from '../src';
+
+describe('useOnMount', () => {
+  const mockEffectCallback = jest.fn();
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should execute once on mount', () => {
+    renderHook(() => useOnMount(mockEffectCallback));
+
+    expect(mockEffectCallback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not execute on unmount', () => {
+    const hook = renderHook(() => useOnMount(mockEffectCallback));
+    expect(mockEffectCallback).toHaveBeenCalledTimes(1);
+    hook.unmount();
+    expect(mockEffectCallback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not execute on rerender', () => {
+    const hook = renderHook(() => useOnMount(mockEffectCallback));
+    expect(mockEffectCallback).toHaveBeenCalledTimes(1);
+    hook.rerender();
+    expect(mockEffectCallback).toHaveBeenCalledTimes(1);
+  });
+});
