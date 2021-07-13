4f679b8862bb82192c9705af84311ab7f2813dee
sdirosa <surai@objectiv.ai>
2021-07-13 22:03:21 +0200

    Integration testing CRA App rewired with Craco+Alias for TS monorepo compatibility

---
+++ tracker/trackers/react/integration/src/index.tsx
@@ -0,0 +1,22 @@
+import { ReactTracker } from "@objectiv/tracker-react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const tracker = new ReactTracker({endpoint: 'http://endpoint'})
+
+console.log(tracker);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
