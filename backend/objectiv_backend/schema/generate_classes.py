225a70a287bb3578e17684c97832bd97a1f4e901
mike <mike@objectiv.ai>
2021-07-22 15:14:53 +0200

    - now generates proper docstrings - added a constructor

--- backend/objectiv_backend/schema/generate_classes.py
+++ backend/objectiv_backend/schema/generate_classes.py
@@ -10,6 +10,7 @@ event_schema = get_config_event_schema()
 def get_type(property_description: Dict[str, str]) -> str:
     type_name = property_description['type']
 
+    abstract_type = ''
     if 'items' in property_description:
         abstract_type = property_description['items']
 
@@ -26,42 +27,146 @@ def get_type(property_description: Dict[str, str]) -> str:
 # first: events
 
 
-def get_classes(objects: List) -> None:
+def get_parent_list(objects: Dict) -> Dict:
+    # first create a list of parent per obj
+    parent_mapping = {}
     for obj_name, obj in objects:
+        if len(obj['parents']) > 0:
+            parent_mapping[obj_name] = obj['parents'][0]
+        else:
+            parent_mapping[obj_name] = None
 
-        properties = []
+    parent_list = {}
+    for klass in parent_mapping.keys():
+        parent = parent_mapping[klass]
+        parent_list[klass] = []
+
+        while True:
+            if parent in parent_mapping:
+                parent_list[klass].append(parent)
+                parent = parent_mapping[parent]
+            else:
+                break
+
+    return parent_list
+
+
+def get_all_properties(object_list: List, objects: Dict) -> Dict:
+    properties = {}
+    for obj in object_list:
+        if 'properties' in objects[obj]:
+            properties.update(objects[obj]['properties'])
+    return properties
+
+
+def get_classes(objects: Dict) -> None:
+
+    pl = get_parent_list(objects.items())
+
+    for obj_name, obj in objects.items():
+
+        hierarchy = [obj_name, *pl[obj_name]]
+        all_properties = get_all_properties(hierarchy, objects=objects)
+
         parent = ''
         parents = obj['parents']
+        super_class = None
+        if len(obj['parents']) > 0:
+            super_class = obj['parents'][0]
         if re.match('^Abstract', obj_name):
             parents.append('ABC')
-        if 'parents' in obj and len(parents) > 0:
+        if len(parents) > 0:
             parent = f' ({", ".join(parents)})'
-        if 'properties' in obj:
 
-            for property_name, property_description in obj['properties'].items():
-                p = '# ' + '\n    # '.join([s.strip() for s in property_description['description'].split('\n')]) + '\n'
-                p += f'    {property_name}: {get_type(property_description)}'
-                properties.append(p)
-        if len(properties) == 0:
-            properties = ['pass']
+        class_description = [s.strip() for s in obj['description'].split('\n')]
 
-        property_string = '\n    '.join(properties)
+        # constructor args, these should include all instance variables, both for this instance,
+        # but also for the super class
+        args = []
+        # instance variables, for this instance
+        iv = []
+        # args to pass to super
+        super_args = []
 
-        description = '\n#  '.join([s.strip() for s in obj['description'].split('\n')])
+        # properties of _this_ instance
+        properties = {}
+        if 'properties' in obj and len(obj['properties']) > 0:
+            properties = obj['properties']
+
+        for property_name in properties.keys():
+            if property_name not in ['_context_type', 'event']:
+                iv.append(f'self.{property_name} = {property_name}')
+
+        cda = []
+        if len(all_properties) > 0:
+            class_description.append('\n    Attributes:')
+            for property_name, property_description in all_properties.items():
+                if property_name in ['_context_type', 'event']:
+                    # ignore these properties, as they are reflected in the class name
+                    continue
+                if property_name not in properties:
+                    super_args.append(f'{property_name}={property_name}')
+                property_type = get_type(property_description)
+
+                args.append(f'{property_name}: {property_type}')
+                cda.append(f':param {property_name}: {property_type} -\n           {property_description["description"].strip()}')
+
+                class_description.append(
+                    f'    {property_name} ({property_type}):\n            {property_description["description"].strip()}')
+
+        instance_variables = '\n        '.join(iv)
+        if len(iv) > 0:
+            instance_variables += '\n'
+
+        args_string = ''
+        if len(args) > 0:
+            if len(args) > 3:
+                args_string = ',\n                 ' + \
+                    ', \n                 '.join(args)
+            else:
+                args_string = ', ' + ', '.join(args)
+
+        constructor_description = (
+            f'        """\n'
+            f'        ' + '\n        '.join(cda) +
+            f'\n        """\n'
+        )
+
+        super_args_string = ''
+        if len(super_args) > 0:
+            if len(super_args) > 3:
+                super_args_string = ',\n                       ' + \
+                    ', \n                       '.join(super_args)
+            else:
+                super_args_string = ', ' + ', '.join(super_args)
+
+        call_super_class = ''
+        if super_class:
+            call_super_class = f'        {super_class}.__init__(self{super_args_string})\n'
+
+        constructor = (
+            f'def __init__(self{args_string}):\n'
+            f'{constructor_description}\n'
+            f'{call_super_class}'
+            f'        {instance_variables}'
+        )
+
+        description = '\n    '.join(class_description)
         tpl = (
-            f'# {description}\n'
+
             f'class {obj_name}{parent}:\n'
-            f'    {property_string}\n'
-            f'\n'
+            '    """\n'
+            f'    {description}\n'
+            '    """\n'
+            f'    {constructor}\n'
         )
 
         print(tpl)
 
 
-print('from typing import List, Dict')
+print('from typing import List')
 print('from abc import ABC\n\n')
 # print(event_schema.contexts.schema.items())
-get_classes(event_schema.contexts.schema.items())
+get_classes(event_schema.contexts.schema)
 
-get_classes(event_schema.events.schema.items())
-
+get_classes(event_schema.events.schema)
