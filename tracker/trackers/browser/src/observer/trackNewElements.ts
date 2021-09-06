5f41c3e7bb89909b0bb2189652767b494b120330
sdirosa <surai@objectiv.ai>
2021-09-06 10:51:47 +0200

    Better performing children tracking

--- tracker/trackers/browser/src/observer/trackNewElements.ts
+++ tracker/trackers/browser/src/observer/trackNewElements.ts
@@ -7,15 +7,15 @@ import trackNewElement from './trackNewElement';
  * Given a Mutation Observer node containing newly added nodes it will track visibility and attach events to them:
  */
 function trackNewElements(element: Element, tracker: BrowserTracker = window.objectiv.tracker) {
-  // Process `track` attributes
   const trackedElements = element.querySelectorAll(`[${TrackingAttribute.context}]`);
-  [element, ...Array.from(trackedElements)].forEach((element) => trackNewElement(element, tracker));
+  [element, ...Array.from(trackedElements)].forEach((element) => {
+    // Process `track` attributes
+    trackNewElement(element, tracker)
 
-  // Process `trackChildren` attributes
-  const childrenTrackedElements = processChildrenTrackingElement(element);
-
-  childrenTrackedElements.forEach((queriedElement) => {
-    trackNewElement(queriedElement, tracker);
+    // Process `trackChildren` attributes
+    processChildrenTrackingElement(element).forEach((queriedElement) => {
+      trackNewElement(queriedElement, tracker);
+    });
   });
 }
 
