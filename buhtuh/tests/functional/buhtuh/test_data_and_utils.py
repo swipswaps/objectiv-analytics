1f4d708b0664e8c5923110a2444f1e92a225ef98
hendrik-obj <hendrik@objectiv.ai>
2021-10-14 10:15:27 +0200

    remove some wip df loading custom dtype stuff

--- buhtuh/tests/functional/buhtuh/test_data_and_utils.py
+++ buhtuh/tests/functional/buhtuh/test_data_and_utils.py
@@ -66,7 +66,7 @@ RAILWAYS_COLUMNS = ['station_id', 'town', 'station', 'platforms']
 RAILWAYS_INDEX_AND_COLUMNS = ['_index_station_id'] + RAILWAYS_COLUMNS
 
 
-def _get_bt(table, dataset, columns, convert_objects, dtype_override=None) -> BuhTuhDataFrame:
+def _get_bt(table, dataset, columns, convert_objects) -> BuhTuhDataFrame:
     engine = sqlalchemy.create_engine(DB_TEST_URL)
     import pandas as pd
     df = pd.DataFrame.from_records(dataset, columns=columns)
@@ -78,10 +78,7 @@ def _get_bt(table, dataset, columns, convert_objects, dtype_override=None) -> Bu
     if 'date' in df.columns:
         df['date'] = df['date'].astype('datetime64')
 
-    buh_tuh = BuhTuhDataFrame.from_dataframe(df, table, engine,
-                                             convert_objects=convert_objects,
-                                             if_exists='replace',
-                                             dtype_override=dtype_override)
+    buh_tuh = BuhTuhDataFrame.from_dataframe(df, table, engine, convert_objects=convert_objects, if_exists='replace')
     return buh_tuh
 
 
