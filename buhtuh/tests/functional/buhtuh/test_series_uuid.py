aa88df6e259830d163dddc75d9366d56e61a44a2
Tom Jansen <tom@getobjectiv.com>
2021-10-19 15:02:32 +0200

    Support UUID <-> string comparison and conversion operations.

--- buhtuh/tests/functional/buhtuh/test_series_uuid.py
+++ buhtuh/tests/functional/buhtuh/test_series_uuid.py
@@ -5,6 +5,7 @@ import uuid
 from unittest.mock import ANY
 
 import pytest
+from psycopg2 import DataError
 
 from buhtuh import BuhTuhSeriesUuid
 from tests.functional.buhtuh.test_data_and_utils import get_bt_with_test_data, assert_equals_data, run_query
@@ -13,13 +14,25 @@ from tests.functional.buhtuh.test_data_and_utils import get_bt_with_test_data, a
 def test_uuid_value_to_sql():
     bt = get_bt_with_test_data()[['city']]
     bt['x'] = uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264')
+    bt['y'] = '0022c7dd-074b-4a44-a7cb-b7716b668264'
+    bt['yy'] = bt.y.astype('uuid')
+    bt['z'] = bt.x == bt.y
+    bt['zz'] = bt.x != '0022c7da-074b-4a44-a7cb-b7716b668263'
+    bt['zzz'] = bt.x == bt.yy
+
     assert_equals_data(
         bt,
-        expected_columns=['_index_skating_order', 'city', 'x'],
+        expected_columns=['_index_skating_order', 'city', 'x', 'y', 'yy', 'z', 'zz', 'zzz'],
         expected_data=[
-            [1, 'Ljouwert', uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264')],
-            [2, 'Snits', uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264')],
-            [3, 'Drylts', uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264')]
+            [1, 'Ljouwert', uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264'),
+             '0022c7dd-074b-4a44-a7cb-b7716b668264',
+             uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264'), True, True, True],
+            [2, 'Snits', uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264'),
+             '0022c7dd-074b-4a44-a7cb-b7716b668264',
+             uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264'), True, True, True],
+            [3, 'Drylts', uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264'),
+             '0022c7dd-074b-4a44-a7cb-b7716b668264',
+             uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264'), True, True, True],
         ]
     )
 
@@ -74,7 +87,7 @@ def test_uuid_generate_random_uuid():
 
 
 def test_uuid_compare():
-    bt = get_bt_with_test_data()[['city']]
+    bt = get_bt_with_test_data()[['city', 'founding']]
     bt['a'] = uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264')
     bt['b'] = uuid.UUID('0022c7dd-074b-4a44-a7cb-b7716b668264')
     bt['c'] = BuhTuhSeriesUuid.sql_gen_random_uuid(bt)
@@ -89,19 +102,28 @@ def test_uuid_compare():
     bt['y'] = bt['b'] != bt['c']
     bt['z'] = bt['c'] != bt['d']  # see comment above
 
+    # non-happy path: compare with a different type, but it's a string so we can only detect failure
+    # on query time. As city will not convert to uuid, this will raise hell
+    with pytest.raises(Exception):
+        dies = bt['b'] == bt['city']
+        dies.head()
 
-    # non-happy path: compare with a different type
+    # if you really want False, you should
+    bt['u'] = bt['b'].astype('string') == bt['city']
+
+    # non-happy path: compare with a different type that not convertable to uuid
     with pytest.raises(TypeError):
-        bt['u'] = bt['b'] == bt['city']
+        bt['v'] = bt['b'] == bt['founding']
 
     # clear long columns, and check results
-    bt = bt[['city', 'w', 'x', 'y', 'z']]
+    bt = bt[['city', 'w', 'x', 'y', 'z', 'u']]
     assert_equals_data(
         bt,
-        expected_columns=['_index_skating_order', 'city', 'w', 'x', 'y', 'z'],
+        expected_columns=['_index_skating_order', 'city', 'w', 'x', 'y', 'z', 'u'],
         expected_data=[
-            [1, 'Ljouwert', True, False, True, True],
-            [2, 'Snits', True, False, True, True],
-            [3, 'Drylts', True, False, True, True],
+            [1, 'Ljouwert', True, False, True, True, False],
+            [2, 'Snits', True, False, True, True, False],
+            [3, 'Drylts', True, False, True, True, False],
         ]
     )
+
