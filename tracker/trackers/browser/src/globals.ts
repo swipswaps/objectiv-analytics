ec12a35271a516f55282a5f08a29bb58575d0621
sdirosa <surai@objectiv.ai>
2021-10-25 14:34:31 +0200

    Create global namespace for all global APIs

--- tracker/trackers/browser/src/globals.ts
+++ tracker/trackers/browser/src/globals.ts
@@ -1,8 +1,6 @@
 import { TrackerRepository } from '@objectiv/tracker-core';
 import { windowExists } from './helpers';
-import { startAutoTracking } from './observer/startAutoTracking';
-import { FlushQueueOptions, WaitForQueueOptions } from './structs';
-import { BrowserTracker, BrowserTrackerConfig } from './tracker/BrowserTracker';
+import { BrowserTracker } from './tracker/BrowserTracker';
 
 /**
  * The interface of our namespace which will be extending the Window interface
@@ -28,89 +26,3 @@ if (windowExists()) {
     trackers: new TrackerRepository(),
   };
 }
-
-/**
- * Retrieves the TrackerRepository instance from the window.objectiv global namespace
- */
-export const getTrackerRepository = (): TrackerRepository<BrowserTracker> => {
-  if (!windowExists()) {
-    throw new Error('Cannot access the Window interface.');
-  }
-
-  return window.objectiv.trackers;
-};
-
-/**
- * Allows to easily create and configure a new BrowserTracker instance and also starts auto tracking
- */
-export const makeTracker = (trackerConfig: BrowserTrackerConfig): BrowserTracker => {
-  const newTracker = new BrowserTracker(trackerConfig);
-  const trackerRepository = getTrackerRepository();
-
-  trackerRepository.add(newTracker);
-  startAutoTracking(trackerConfig);
-
-  return newTracker;
-};
-
-/**
- * Retrieves a specific instance of the tracker from the TrackerRepository.
- */
-export const getTracker = (trackerId?: string): BrowserTracker => {
-  const tracker = getTrackerRepository().get(trackerId);
-
-  // Throw if we did not manage to get a tracker instance
-  if (!tracker) {
-    throw new Error('No Tracker found. Please create one via `makeTracker`.');
-  }
-
-  return tracker;
-};
-
-/**
- * Helper method to easily set a different default Tracker in the TrackerRepository.
- */
-export const setDefaultTracker = async (
-  parameters:
-    | string
-    | {
-        trackerId: string;
-        waitForQueue?: false | WaitForQueueOptions;
-        flushQueue?: FlushQueueOptions;
-      }
-) => {
-  let trackerId: string;
-  let waitForQueue: undefined | WaitForQueueOptions;
-  let flushQueue: undefined | FlushQueueOptions;
-
-  // Some sensible defaults
-  const defaultWaitForQueue = {}; // Wait for Queue with default options before switching default Tracker.
-  const defaultFlushQueue = true; // Flush the Queue before switching default Tracker.
-
-  if (typeof parameters === 'string') {
-    trackerId = parameters;
-    waitForQueue = defaultWaitForQueue;
-    flushQueue = defaultFlushQueue;
-  } else {
-    trackerId = parameters.trackerId;
-    waitForQueue = parameters.waitForQueue ?? defaultWaitForQueue;
-    flushQueue = parameters.flushQueue ?? defaultFlushQueue;
-  }
-
-  // Get current default Tracker
-  const tracker = getTracker();
-
-  // Process waitForQueue
-  let isQueueEmpty = true;
-  if (waitForQueue) {
-    isQueueEmpty = await tracker.waitForQueue(waitForQueue);
-  }
-
-  // Process flushQueue
-  if (flushQueue === true || (flushQueue === 'onTimeout' && !isQueueEmpty)) {
-    tracker.flushQueue();
-  }
-
-  // Set the new default Tracker
-  getTrackerRepository().setDefault(trackerId);
-};
