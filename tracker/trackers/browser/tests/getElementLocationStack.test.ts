9726f58aff19f9cf9d098a22f752baaa71a0e4a2
sdirosa <surai@objectiv.ai>
2021-10-27 09:52:47 +0200

    Coverage for getElementLocationStack - full test suite to be done

---
+++ tracker/trackers/browser/tests/getElementLocationStack.test.ts
@@ -0,0 +1,17 @@
+import { getElementLocationStack } from '../src/tracker/getElementLocationStack';
+
+describe('getElementLocationStack', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    document.getElementsByTagName('html')[0].innerHTML = '';
+  });
+
+  it('should console.error', () => {
+    jest.spyOn(console, 'error')
+    // @ts-ignore
+    expect(getElementLocationStack(null)).toHaveLength(0);
+    expect(console.error).toHaveBeenCalledTimes(1);
+  });
+
+  // TODO exhaustive testing
+});
