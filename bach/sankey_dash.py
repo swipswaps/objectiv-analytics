4a6296908f1292e4ceb58227dd3ef7a41fa00ec6
hendrik-obj <hendrik@objectiv.ai>
2021-11-05 17:51:46 +0100

    create features with sankey

--- bach/sankey_dash.py
+++ bach/sankey_dash.py
@@ -42,8 +42,16 @@ def get_app(Dash, feature_frame, start_event_dropdown_option='all', start_stack_
             html.Label('Slice:'),
             dcc.Input(id='start-at', type='number', min=0, step=1),
             dcc.Input(id='end-at', type='number', step=1),
+            html.Br(),
+            dcc.Input(id='feature-name', type='text', placeholder='Feature name'),
+            html.Button('Create feature', id='create-feature', n_clicks=0),
+            html.Br(),
+            html.Button('Reset feature rules', id='reset-feature-rules', n_clicks=0),
             dcc.Graph(id='top-graph'),
-            html.Button('Reset feature rules', id='reset-feature-rules', n_clicks=0)
+            dcc.Markdown(d("**Feature rules**")),
+            html.Pre(id='feature-rules', style=styles['pre']),
+            dcc.Markdown(d("**Console**")),
+            html.Pre(id='console', style=styles['pre'])
         ])
 
 
@@ -96,6 +104,84 @@ def get_app(Dash, feature_frame, start_event_dropdown_option='all', start_stack_
             return filtered_feature_frame.display_sankey('_sankey_feature')
         else:
             return filtered_feature_frame[filtered_feature_frame[filtered_feature_frame.event_column]==event_value].display_sankey('_sankey_feature')
+
+    @app.callback(
+        Output('feature-rules', 'children'),
+        [Input('node-clicks', 'data'),
+         Input('start-at', 'value'),
+         Input('end-at', 'value')])
+    def display_feature_rule_real(node_clicks_data, start_at_value, end_at_value):
+        feature_rules = [f'.json[{x}:]' for x in node_clicks_data]
+        if not (start_at_value is None and end_at_value is None):
+            feature_rules.append(f'.json[{start_at_value}:{end_at_value}]')
+
+        return '\\\n'.join(feature_rules)
+
+    @app.callback(
+        [Output('stack', 'options'),
+         Output('console', 'children')],
+        Input('create-feature', 'n_clicks'),
+        [State('node-clicks', 'data'),
+         State('stack', 'value'),
+         State('start-at', 'value'),
+         State('end-at', 'value'),
+         State('feature-name', 'value')])
+    def create_feature(
+            create_feature_n_clicks,
+            node_clicks_data,
+            stack_value,
+            start_at_value,
+            end_at_value,
+            feature_name_value):
+        """
+        Creates a feature based clicks in the top graph, slice and
+        name.
+
+        Parameters
+        ----------
+        create_feature_n_clicks : int
+            Current number of clicks on the Create Feature button.
+
+        node_clicks_data : List[dict]
+            Each element in the list contains the name of the node
+            that will be used to create a feature. Each clicked node
+            will be the start of a feature.
+
+        feature_name_value : str
+            The name of the feature that will be used once a feature is
+            created.
+
+        start_at_value : int
+            Optionally a starting position of the feature can be
+            specified. This rule will always be applied after all
+            the rules in nodes have been applied.
+            The function for aggregating the data.
+
+        end_at_value : int
+            See start_at_value. This will be applied together
+            with start_at_value.
+
+        """
+        console_text = ''
+        prop_id_triggered = callback_context.triggered[0]['prop_id']
+        if prop_id_triggered == 'create-feature.n_clicks':
+            feature_frame[feature_name_value] = feature_frame[stack_value]
+            if len(node_clicks_data) > 0:
+                for x in node_clicks_data:
+                    feature_frame[feature_name_value] = feature_frame[stack_value].json[x:]
+            if start_at_value is not None or end_at_value is not None:
+                feature_frame[feature_name_value] = feature_frame[feature_name_value].json[start_at_value:end_at_value]
+
+            if not feature_name_value in [x['label'] for x in stack_dropdown_options]:
+                stack_dropdown_options.append({'label': feature_name_value, 'value': feature_name_value})
+            console_text = f'feature {feature_name_value} created'
+
+        return stack_dropdown_options, console_text
+
+
+
+
+
 
     @app.callback(
         Output('node-clicks', 'data'),
