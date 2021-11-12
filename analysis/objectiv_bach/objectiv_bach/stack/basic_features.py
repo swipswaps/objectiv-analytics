7fd6ec0929a6f6f5abff9995d9aef19eb12a5069
vincent.hoogsteder <vincent@objectiv.io>
2021-11-12 10:29:59 +0100

    taking out un-needed columns from basic features and remaning event_types

--- analysis/objectiv_bach/objectiv_bach/stack/basic_features.py
+++ analysis/objectiv_bach/objectiv_bach/stack/basic_features.py
@@ -10,8 +10,18 @@ class BasicFeatures(SqlModelBuilder):
     def sql(self):
         return '''
 SELECT
-  data.*,
-  bm.feature
-FROM {{sessionized_data}} as data
+  sd.event_id,
+  sd.day,
+  sd.moment,
+  sd.session_id,
+  sd.session_hit_number,
+  sd.user_id,
+  sd.global_contexts,
+  sd.location_stack,
+  sd.event_type,
+  sd.stack_event_types,
+  bm.feature,
+  bm.feature_hash
+FROM {{sessionized_data}} sd
 JOIN {{feature_table}} bm USING (feature_hash)
 '''
