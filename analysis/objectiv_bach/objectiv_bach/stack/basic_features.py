0ef125fa372ccc23f1b53ee5c53185a156c6daeb
hendrik-obj <hendrik@objectiv.ai>
2021-11-18 11:59:36 +0100

    remove more obsolete sqlmodels from basic_features

--- analysis/objectiv_bach/objectiv_bach/stack/basic_features.py
+++ analysis/objectiv_bach/objectiv_bach/stack/basic_features.py
@@ -9,19 +9,6 @@ class BasicFeatures(SqlModelBuilder):
     @property
     def sql(self):
         return '''
-SELECT
-  sd.event_id,
-  sd.day,
-  sd.moment,
-  sd.session_id,
-  sd.session_hit_number,
-  sd.user_id,
-  sd.global_contexts,
-  sd.location_stack,
-  sd.event_type,
-  sd.stack_event_types,
-  bm.feature,
-  bm.feature_hash
+SELECT *
 FROM {{sessionized_data}} sd
-JOIN {{feature_table}} bm USING (feature_hash)
 '''
