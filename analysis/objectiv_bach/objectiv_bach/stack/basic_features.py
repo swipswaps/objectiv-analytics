42ae202c25d784838147009a14f8514505a75856
hendrik-obj <hendrik@objectiv.ai>
2021-11-18 12:09:49 +0100

    remove cookie_id in basic features

--- analysis/objectiv_bach/objectiv_bach/stack/basic_features.py
+++ analysis/objectiv_bach/objectiv_bach/stack/basic_features.py
@@ -9,6 +9,16 @@ class BasicFeatures(SqlModelBuilder):
     @property
     def sql(self):
         return '''
-SELECT *
+SELECT
+  sd.event_id,
+  sd.day,
+  sd.moment,
+  sd.session_id,
+  sd.session_hit_number,
+  sd.user_id,
+  sd.global_contexts,
+  sd.location_stack,
+  sd.event_type,
+  sd.stack_event_types
 FROM {{sessionized_data}} sd
 '''
