85ede07162a1b22435f22cc1a7780fca317bb272
sdirosa <surai@objectiv.io>
2021-12-27 14:35:31 +0100

    PathContextFromURLPlugin

--- tracker/plugins/path-context-from-url/jest.config.js
+++ tracker/plugins/path-context-from-url/jest.config.js
@@ -8,9 +8,6 @@ module.exports = {
   reporters: ['jest-standard-reporter'],
   collectCoverageFrom: ['src/**.ts'],
   moduleNameMapper: {
-    '@objectiv/plugin-(.*)': '<rootDir>../../plugins/$1/src',
-    '@objectiv/schema': '<rootDir>../../core/schema/src',
-    '@objectiv/testing-tools': '<rootDir>../../core/testing-tools/src',
     '@objectiv/tracker-core': '<rootDir>../../core/tracker/src',
   },
   setupFilesAfterEnv: ['jest-extended/all'],
--- tracker/plugins/web-document-context/src/index.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright 2021 Objectiv B.V.
- */
-
-export * from './DocumentLoadedEvent';
-export * from './WebDocumentContextPlugin';
