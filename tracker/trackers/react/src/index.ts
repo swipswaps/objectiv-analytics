2097869136b9fd6e1ea2a6f4c754a5785553ef68
sdirosa <surai@objectiv.io>
2022-01-06 15:50:29 +0100

    Remove unused function

--- tracker/trackers/react/src/index.ts
+++ tracker/trackers/react/src/index.ts
@@ -7,6 +7,5 @@ export * from '@objectiv/tracker-core-react';
 export * from './common/factories/makeDefaultPluginsList';
 export * from './common/factories/makeDefaultQueue';
 export * from './common/factories/makeDefaultTransport';
-export * from './common/executeOnce';
 
 export * from './ReactTracker';
