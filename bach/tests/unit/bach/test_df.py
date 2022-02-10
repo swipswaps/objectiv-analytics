62eab1e2d95b2ab82679c31802eb8b1e80183546
thijs <thijs@getobjectiv.com>
2022-02-10 17:01:19 +0100

    Unit test for the DataFrame.__init__() checks

--- bach/tests/unit/bach/test_df.py
+++ bach/tests/unit/bach/test_df.py
@@ -1,8 +1,14 @@
 """
 Copyright 2021 Objectiv B.V.
 """
-from bach import SortColumn
+import pytest
+
+from bach import SortColumn, DataFrame
 from bach.expression import Expression
+from bach.partitioning import GroupBy
+from bach.savepoints import Savepoints
+from bach.sql_model import BachSqlModel
+from sql_models.model import CustomSqlModelBuilder
 from tests.unit.bach.util import get_fake_df
 
 
@@ -53,3 +59,96 @@ def test__eq__():
     left = left.set_variable('a', '1234')
     right = right.set_variable('a', 1234)
     assert left == right
+
+
+def test_init_conditions():
+    df = get_fake_df(['a'], ['b', 'c'])
+    df2 = get_fake_df(['a', 'b'], ['c', 'd'])
+    other_base_node = BachSqlModel.from_sql_model(
+        sql_model=CustomSqlModelBuilder('select * from y', name='base')(),
+        columns=('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+    )
+
+    # Check that with 'normal' parameters the __init__ function does not complain
+    DataFrame(
+        engine=df.engine,
+        base_node=df.base_node,
+        index=df.index,
+        series=df.data,
+        group_by=None,
+        order_by=[],
+        savepoints=Savepoints()
+    )
+
+    # Now check that with wrong values __init__ raises ValueErrors
+    with pytest.raises(ValueError, match="Indices in `series` should match dataframe"):
+        DataFrame(
+            engine=df.engine,
+            base_node=df.base_node,
+            index=df2.index,
+            series=df.data,
+            group_by=None,
+            order_by=[],
+            savepoints=Savepoints()
+        )
+
+    with pytest.raises(ValueError, match="Keys in `series` should match the name of series"):
+        DataFrame(
+            engine=df.engine,
+            base_node=df.base_node,
+            index=df.index,
+            series={k + 'suffix': v for k, v in df.data.items()},
+            group_by=None,
+            order_by=[],
+            savepoints=Savepoints()
+        )
+
+    with pytest.raises(ValueError, match="Group_by in `series` should match dataframe"):
+        DataFrame(
+            engine=df.engine,
+            base_node=df.base_node,
+            index=df.index,
+            series=df.data,
+            group_by=GroupBy([df.b]),
+            order_by=[],
+            savepoints=Savepoints()
+        )
+
+    with pytest.raises(ValueError, match="Base_node in `series` should match dataframe"):
+        DataFrame(
+            engine=df.engine,
+            base_node=other_base_node,
+            index=df.index,
+            series=df.data,
+            group_by=None,
+            order_by=[],
+            savepoints=Savepoints()
+        )
+
+    # df2.c has an index of itself
+    other_index = {'a': df2.c}
+    with pytest.raises(ValueError, match="Index series can not have non-empty index property"):
+        DataFrame(
+            engine=df.engine,
+            base_node=df.base_node,
+            index=other_index,
+            series={k: v.copy_override(index=other_index) for k, v in df.data.items()},
+            group_by=None,
+            order_by=[],
+            savepoints=Savepoints()
+        )
+
+    other_index = {'b': df.index['a']}
+    with pytest.raises(ValueError,
+                       match="The names of the index series and data series should not intersect"):
+        DataFrame(
+            engine=df.engine,
+            base_node=df.base_node,
+            index=other_index,
+            series={k: v.copy_override(index=other_index) for k, v in df.data.items()},
+            group_by=None,
+            order_by=[],
+            savepoints=Savepoints()
+        )
+    # there are a few __init__ checks that we don't check here, as they are also checked when creating a
+    # Series object, and are thus hard to actually trigger.
