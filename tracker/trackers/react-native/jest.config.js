f1782bd2fe7e88370efc68192e25799b2088c5cb
sdirosa <surai@objectiv.io>
2022-03-02 14:03:48 +0100

    Basic package setup for react-native

---
+++ tracker/trackers/react-native/jest.config.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2021-2022 Objectiv B.V.
+ */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  reporters: ['jest-standard-reporter'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  moduleNameMapper: {
+    '@objectiv/plugin-(.*)': '<rootDir>/../../plugins/$1/src',
+    '@objectiv/queue-(.*)': '<rootDir>/../../queues/$1/src',
+    '@objectiv/testing-tools': '<rootDir>../../core/testing-tools/src',
+    '@objectiv/tracker-core': '<rootDir>/../../core/tracker/src',
+    '@objectiv/transport-(.*)': '<rootDir>/../../transports/$1/src',
+  },
+};
---
+++ tracker/trackers/react-native/src/index.ts
@@ -0,0 +1,6 @@
+/*
+ * Copyright 2021-2022 Objectiv B.V.
+ */
+
+// TODO re-export from react-tracker
+
