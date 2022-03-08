8238b8b1342a69c3d7a5eae036a509ff0b04dbf5
sdirosa <surai@objectiv.io>
2022-03-08 20:40:23 +0100

    Add mapping for plugin and queues to React Router plugin test

--- tracker/plugins/react-router-tracked-components/jest.config.js
+++ tracker/plugins/react-router-tracked-components/jest.config.js
@@ -8,6 +8,9 @@ module.exports = {
   reporters: ['jest-standard-reporter'],
   collectCoverageFrom: ['src/**.ts'],
   moduleNameMapper: {
+    '@objectiv/plugin-(.*)': '<rootDir>/../../plugins/$1/src',
+    '@objectiv/queue-(.*)': '<rootDir>/../../queues/$1/src',
+    '@objectiv/transport-(.*)': '<rootDir>/../../transport/$1/src',
     '@objectiv/testing-tools': '<rootDir>../../core/testing-tools/src',
     '@objectiv/tracker-core-react': '<rootDir>../../core/react/src',
     '@objectiv/tracker-core': '<rootDir>../../core/tracker/src',
