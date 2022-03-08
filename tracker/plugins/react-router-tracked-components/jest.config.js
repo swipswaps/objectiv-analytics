bc3b4032f157e9906704892a073cdc2cb372baa0
sdirosa <surai@objectiv.io>
2022-03-08 20:42:29 +0100

    Map transports for react-router plugin tests

--- tracker/plugins/react-router-tracked-components/jest.config.js
+++ tracker/plugins/react-router-tracked-components/jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
   moduleNameMapper: {
     '@objectiv/plugin-(.*)': '<rootDir>/../../plugins/$1/src',
     '@objectiv/queue-(.*)': '<rootDir>/../../queues/$1/src',
-    '@objectiv/transport-(.*)': '<rootDir>/../../transport/$1/src',
+    '@objectiv/transport-(.*)': '<rootDir>/../../transports/$1/src',
     '@objectiv/testing-tools': '<rootDir>../../core/testing-tools/src',
     '@objectiv/tracker-core-react': '<rootDir>../../core/react/src',
     '@objectiv/tracker-core': '<rootDir>../../core/tracker/src',
