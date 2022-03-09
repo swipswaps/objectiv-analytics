b90add87b25444dcbeb2ab2fe84a38fc85d49f85
sdirosa <surai@objectiv.io>
2022-03-09 20:46:47 +0100

    Validation in PathContext

--- tracker/plugins/path-context-from-url/src/PathContextFromURLPlugin.ts
+++ tracker/plugins/path-context-from-url/src/PathContextFromURLPlugin.ts
@@ -4,25 +4,42 @@
 
 import {
   ContextsConfig,
+  GlobalContextValidationRule,
   makePathContext,
   TrackerConsole,
+  TrackerEvent,
   TrackerPluginConfig,
   TrackerPluginInterface,
+  TrackerValidationRuleInterface,
 } from '@objectiv/tracker-core';
 
 /**
  * The PathContextFromURL Plugin gathers the current URL using the Location API.
  * It implements the `enrich` lifecycle method. This ensures the URL is retrieved before each Event is sent.
+ *
+ * Event Validation:
+ *  - Must be present in Global Contexts
+ *  - Must not be present multiple times
  */
 export class PathContextFromURLPlugin implements TrackerPluginInterface {
   readonly console?: TrackerConsole;
   readonly pluginName = `PathContextFromURLPlugin`;
+  readonly validationRules: TrackerValidationRuleInterface[];
 
   /**
    * The constructor is merely responsible for processing the given TrackerPluginConfiguration `console` parameter.
    */
   constructor(config?: TrackerPluginConfig) {
     this.console = config?.console;
+    this.validationRules = [
+      new GlobalContextValidationRule({
+        console: this.console,
+        logPrefix: this.pluginName,
+        contextName: 'PathContext',
+        once: true,
+      }),
+    ];
+
     if (this.console) {
       this.console.log(`%c｢objectiv:${this.pluginName}｣ Initialized`, 'font-weight: bold');
     }
@@ -36,6 +53,15 @@ export class PathContextFromURLPlugin implements TrackerPluginInterface {
       id: document.location.href,
     });
     contexts.global_contexts.push(pathContext);
+  }
+
+  /**
+   * If the Plugin is usable runs all validation rules.
+   */
+  validate(event: TrackerEvent): void {
+    if (this.isUsable()) {
+      this.validationRules.forEach((validationRule) => validationRule.validate(event));
+    }
   }
 
   /**
