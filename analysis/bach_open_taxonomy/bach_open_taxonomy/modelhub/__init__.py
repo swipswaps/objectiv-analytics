536b76612ac308af1fa5ebdd4b41347af13d2f6c
Tom Jansen <tom@getobjectiv.com>
2022-03-16 11:31:57 +0100

    Restructure Modelhub repo section: objectiv-analytics │ └─bach │ └─modelhub │   │ │   │ │   └──modelhub │   │   │   modelhub.py │   │   │   aggregate.py │   │   │   map.py │   │   │ │   │   └──stack │   │   │       basic_features.py │   │   │       extracted_contexts.py │   │   │       sessionized_data.py │   │   │       util.py │   │   │ │   │   └──util │   │           sankey_dash.py │   │           metabase.py (metabase class is currently in series_objectiv.py) │   │ │   └──series_objectiv │   │       series_objectiv.py │   │ │   └──tests │ └──notebooks
    wtih that, update notebooks docker to run from /notebooks, and hopefully update all / most references.
    Docs will be fixed in later commit.

--- analysis/bach_open_taxonomy/bach_open_taxonomy/modelhub/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Copyright 2021 Objectiv B.V.
-"""
-from bach_open_taxonomy.modelhub.modelhub import ModelHub
-from bach_open_taxonomy.modelhub.map import Map
-from bach_open_taxonomy.modelhub.aggregate import Aggregate
---
+++ modelhub/modelhub/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright 2021 Objectiv B.V.
+"""
+__version__ = '0.0.1'
+
+from modelhub.modelhub import ModelHub
+from modelhub.aggregate import Aggregate
+from modelhub.map import Map
+from modelhub.stack import *
+from modelhub.series import *
