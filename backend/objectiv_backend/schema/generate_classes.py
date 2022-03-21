dda4423188de14a659c83a9a7b127b11901a6501
mike <mike@objectiv.io>
2022-03-21 17:16:34 +0100

    also fix type mapping for objects -> Dict

--- backend/objectiv_backend/schema/generate_classes.py
+++ backend/objectiv_backend/schema/generate_classes.py
@@ -16,9 +16,9 @@ def get_type(property_description: Dict[str, Any]) -> str:
     if 'items' in property_description and 'type' in property_description['items']:
         abstract_type = get_type({'type': property_description['items']['type']})
     if type_name == 'array':
-        return f'List[{abstract_type}]'
+        return f'List[]'
     if type_name == 'object':
-        return 'Dict[str, Any]'
+        return f'Dict[{abstract_type}, Any]'
     if type_name == 'string':
         return 'str'
     if type_name == 'integer':
