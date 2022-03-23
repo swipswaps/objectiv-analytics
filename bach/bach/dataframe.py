6671a9dc872fda7a2cc69aa9d68b755bdfc3fbd6
Kathia Barahona <katbara03@gmail.com>
2022-03-23 11:53:00 +0100

    small fixes, more tests

--- bach/bach/dataframe.py
+++ bach/bach/dataframe.py
@@ -3074,8 +3074,12 @@ class DataFrame:
         if not columns_to_encode:
             return self.copy()
 
-        if any(col not in self.data or self.data[col].dtype != 'string' for col in columns_to_encode):
-            raise ValueError('Can only encode string dtype columns.')
+        invalid_columns = [
+            col for col in columns_to_encode
+            if col not in self.data or self.data[col].dtype != 'string'
+        ]
+        if invalid_columns:
+            raise ValueError(f'{invalid_columns} are not valid columns.')
 
         prefix_per_col = {}
         if isinstance(prefix, dict):
@@ -3088,8 +3092,11 @@ class DataFrame:
 
         categorical_series = []
         from bach.series.series import const_to_series
+        df_cp = self.copy()
         for col in columns_to_encode:
-            text_series = self[col]
+            if dummy_na:
+                df_cp.loc[df_cp[col].isnull(), col] = 'nan'
+            text_series = df_cp[col]
             prefix_val = f'{prefix_per_col.get(col, col)}{prefix_sep}'
             prefix_series = const_to_series(text_series, value=prefix_val, name=col)
             text_series = prefix_series + text_series
@@ -3098,16 +3105,18 @@ class DataFrame:
 
         from bach.operations.concat import SeriesConcatOperation
         categorical_df = SeriesConcatOperation(categorical_series)().to_frame()
+        categorical_df = categorical_df.dropna()
         categorical_df['values'] = 1
-        if dummy_na:
-            mask = categorical_df[categorical_df.data_columns[0]].isnull()
-            categorical_df.loc[mask, categorical_df.data_columns[0]] = 'NaN'
-
         categorical_df = categorical_df.set_index(categorical_df.data_columns[0], append=True)
-        dummies_df = categorical_df['values'].unstack(fill_value=0).astype(dtype)
-        remaining_columns = [dc for dc in self.data_columns if dc not in columns_to_encode]
+        dummies_df = categorical_df['values'].unstack()
 
-        return self[remaining_columns].merge(dummies_df, left_index=True, right_index=True)
+        remaining_columns = [dc for dc in self.data_columns if dc not in columns_to_encode]
+        dummy_columns = dummies_df.data_columns
+
+        encoded_df = self[remaining_columns].merge(dummies_df, how='left', left_index=True, right_index=True)
+        encoded_df = encoded_df.fillna(value=dict(zip(dummy_columns, [0] * len(dummy_columns))))
+        encoded_df[dummy_columns] = encoded_df[dummy_columns].astype(dtype)
+        return encoded_df
 
 
 def dict_name_series_equals(a: Dict[str, 'Series'], b: Dict[str, 'Series']):
