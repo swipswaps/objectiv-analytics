cb66d7495e0ac1d8b856faca6c75fc352fd132e9
thijs <thijs@getobjectiv.com>
2022-03-28 13:38:50 +0200

    fix forgotten tests

--- bach/tests/unit/bach/test_df_astype.py
+++ bach/tests/unit/bach/test_df_astype.py
@@ -6,8 +6,8 @@ import numpy
 from tests.unit.bach.util import get_fake_df_test_data
 
 
-def test_astype_dtype_aliases():
-    bt = get_fake_df_test_data()
+def test_astype_dtype_aliases(dialect):
+    bt = get_fake_df_test_data(dialect)
     bt = bt[['inhabitants']]
     # Using an alias of 'int64' in the call to `astype()` should result in the same DataFrame
     bt_int0 = bt.astype('int64')
