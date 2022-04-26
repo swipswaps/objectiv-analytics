60e584b8b899dfe66dd9bccaea0f25ea53b4160c
sdirosa <surai@objectiv.io>
2022-04-26 20:06:51 +0200

    Added cypress, pm2, wait-on and moved CRA tests to cypress

---
+++ tracker/e2e/react-cra/cypress/integration/home.test.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2022 Objectiv B.V.
+ */
+
+it("should load the page", () => {
+  cy.visit("/");
+  cy.findAllByText(/learn react/i).should("have.length", 1);
+});
--- tracker/e2e/react-cra/run.sh
+++ tracker/e2e/react-cra/run.sh
@@ -6,8 +6,15 @@ npm install
 # build
 npm run build
 
+# start the app
+./node_modules/pm2/bin/pm2 start npm --name react-cra -- run start
+
 # run tests
-npm run test -- --watchAll=false
+./node_modules/wait-on/bin/wait-on http://localhost:3000 && npm run test
+
+# stop the app
+./node_modules/pm2/bin/pm2 stop react-cra
+./node_modules/pm2/bin/pm2 delete react-cra
 
 # cleanup
 rm -rf build
