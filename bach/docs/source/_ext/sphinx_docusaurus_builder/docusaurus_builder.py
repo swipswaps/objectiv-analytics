90f2d50e5fac6b45dcb86abe1bb2a413691b9125
Ivar Pruijn <ivar@objectiv.io>
2022-05-09 17:26:40 +0200

    Clean up and document the Builder

--- bach/docs/source/_ext/sphinx_docusaurus_builder/docusaurus_builder.py
+++ bach/docs/source/_ext/sphinx_docusaurus_builder/docusaurus_builder.py
@@ -10,25 +10,29 @@ from sphinx.util.osutil import ensuredir, os_path
 import datetime
 from urllib.parse import quote
 
-if False:
-    from typing import Any, Dict, Iterator, Set, Tuple
-    from docutils import nodes
-    from sphinx.application import Sphinx
-
 logger = logging.getLogger(__name__)
 
 class DocusaurusBuilder(Builder):
+    """Class to build Docusaurus-compatible MDX documentation from Sphinx.
+
+    """
     name = 'docusaurus'
     format = 'docusaurus'
-    epilog = __('The docusaurus files are in %(outdir)s.')
+    epilog = __('The Docusaurus files are in %(outdir)s.')
 
     allow_parallel = True
     default_translator_class = DocusaurusTranslator
-    current_docname = None
+    current_docname = None # the current doc being processed
 
-    out_suffix = '.mdx'
-    link_suffix = '/'
-    markdown_http_base = '/modeling'
+    out_suffix = '.mdx' # file extensions for output
+    link_suffix = '/' # end of URL, e.g. a trailing slash
+    markdown_http_base = '/modeling' # base URL path for all files
+
+    # formatting of frontmatter for the API references
+    # first level specifies the document path, e.g. 'models/api-reference'
+    # second level specifies how far back the path should show:
+    #   * ``title_tree_levels``: e.g. 'models.abc.xyz' with title_tree_levels 2 becomes 'abc.xyz' in the title
+    #   * ``slug_tree_levels``: e.g. 'models.abc' with slug_tree_levels 1 becomes 'abc' in the slug
     api_frontmatter = {
         'open-model-hub/models': {
             'title_tree_levels': 1,
@@ -44,10 +48,19 @@ class DocusaurusBuilder(Builder):
         }
     }
 
+
     def init(self):
         self.secnumbers = {}
 
+
     def get_outdated_docs(self):
+        """
+        Find documents that are outdated and should be built.
+
+        :returns: generator with each docname that is outdated.
+
+        """
+        
         for docname in self.env.found_docs:
             if docname not in self.env.all_docs:
                 yield docname
@@ -64,20 +77,46 @@ class DocusaurusBuilder(Builder):
             except EnvironmentError:
                 pass
 
+
     def get_target_uri(self, docname: str, typ: str = None) -> str:
+        """
+        Generate a URI from the docname and config.
+
+        :returns: Formatted URI
+
+        """
+        
         return quote(docname) + self.link_suffix
 
+
     def prepare_writing(self, docnames):
+        """
+        Prepares documents for writing. Instantiates the writer and context.
+
+        """
+        
         self.writer = DocusaurusWriter(self)
         self.ctx = self.create_context()
 
+
     def create_context(self):
+        """
+        Creates the context for writing documents; currently just with the datetime.
+
+        """
+
         ctx = munchify({
             'date': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
         })
         return ctx
 
+
     def write_doc(self, docname, doctree):
+        """
+        Writes a document to the fileystem.
+
+        """
+
         self.current_docname = docname
         self.secnumbers = self.env.toc_secnumbers.get(docname, {})
         destination = StringOutput(encoding='utf-8')
@@ -89,6 +128,7 @@ class DocusaurusBuilder(Builder):
                 f.write(self.writer.output)
         except (IOError, OSError) as err:
             logger.warning(__("error writing file %s: %s"), outfilename, err)
+
 
     def finish(self):
         pass
