b7724d21d9e03c45d307afe363169ba9c181bc4b
Tom Jansen <10804239+jansentom@users.noreply.github.com>
2022-05-18 15:29:46 +0200

    bump version to v0.0.6

--- bach/bach/__init__.py
+++ bach/bach/__init__.py
@@ -1,7 +1,7 @@
 """
 Copyright 2021 Objectiv B.V.
 """
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 from bach.dataframe import DataFrame, DataFrameOrSeries, ColumnNames, SortColumn, \
     get_series_type_from_dtype
